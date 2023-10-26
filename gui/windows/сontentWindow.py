2357f287da10573d56b3150d9a26da566c56b4b3
lening_da <leningdenis@gmail.com>
2023-10-26 13:01:09 +1000

    add video view

--- gui/windows/сontentWindow.py
+++ gui/windows/сontentWindow.py
@@ -1,7 +1,8 @@
 from PyQt6.QtWidgets import QWidget, QPushButton, QHBoxLayout, QLabel, QFileDialog, QVBoxLayout
-from PyQt6.QtGui import QPixmap
+from PyQt6.QtGui import QPixmap, QImage
 from PyQt6.QtCore import Qt
 
+import cv2
 
 class ContentWindow(QWidget):
     def __init__(self):
@@ -21,19 +22,14 @@ class ContentWindow(QWidget):
 
         button_layout.addWidget(image_button)
         button_layout.addWidget(video_button)
-        
+
         button_widget = QWidget()
         button_widget.setLayout(button_layout)
-    
 
         button_layout.setAlignment(Qt.AlignmentFlag.AlignTop)
 
         self.label = QLabel(self)
         self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
-
-
-
 
         main_layout = QVBoxLayout()
         main_layout.setContentsMargins(0, 0, 0, 20)
@@ -63,21 +59,33 @@ class ContentWindow(QWidget):
                 # self.label.setPixmap(pixmap)
                 # self.label.setScaledContents(True)
                 self.label.setPixmap(pixmap)
-        
+
                 self.label.adjustSize()
                 self.setWindowTitle(filenames[0])
 
     def open_video(self):
-        # dialog = QFileDialog(self)
-        # dialog.setDirectory(r'C:\images')
-        # dialog.setFileMode(QFileDialog.FileMode.ExistingFiles)
-        # dialog.setNameFilter("Videos (*.mp4 *.avi *.mov)")
-        # dialog.setViewMode(QFileDialog.ViewMode.List)
-        # if dialog.exec():
-        #     filenames = dialog.selectedFiles()
-        # if filenames:
-        #     # Load the selected video and play it
-        #     media_content = QMediaContent.fromUrl(filenames[0])
-        #     self.media_player.setMedia(media_content)
-        #     self.media_player.play() 
-        pass
+        dialog = QFileDialog()
+        dialog.setFileMode(QFileDialog.FileMode.ExistingFile)
+
+        if dialog.exec():
+            filename = dialog.selectedFiles()[0]
+
+            cap = cv2.VideoCapture(filename)
+            while cap.isOpened():
+                ret, frame = cap.read()
+
+                if ret:
+                    # Конвертируем кадр в формат QImage
+                    rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                    h, w, ch = rgb_image.shape
+                    bytes_per_line = ch * w
+                    qt_image = QImage(rgb_image.data, w, h, bytes_per_line, QImage.Format.Format_RGB888)
+
+                    # Отображаем кадр в QLabel
+                    pixmap = QPixmap.fromImage(qt_image)
+                    self.label.setPixmap(pixmap)
+
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+
+            cap.release()
